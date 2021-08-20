11bc7c52f9facf7382f127784adc3dcd18d4dd8a
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-08-20 19:07:26 +0200

    updated script.js

--- script.js
+++ script.js
@@ -13,12 +13,26 @@
 // };
 //   window.onload = setup;
 // ####################################################################################################################################
+
 let episodeFrames = document.getElementById('episodeFrames');
-  let searchBar = document.getElementById('searchBar');
+let searchBar = document.getElementById('searchBar');
 let allEpisodes = getAllEpisodes();
 let episodeCode = "";
+let searchResults = document.getElementById('searchResults');
+let selectList = document.getElementById("selects");
+let  homeBtn = document.getElementById("homeBtn");
 
  allEpisodes.forEach(display);
+
+ // creating a dropdown list
+allEpisodes.map((episodes) => {
+  var option = document.createElement("option");
+  option.value = episodes.name;
+  option.text = episodes.name;
+  selectList.appendChild(option);
+});
+
+
 
 // Main Function
 function display(allEpisodes) {
@@ -39,9 +53,9 @@ function display(allEpisodes) {
   // The Div
   let episodeDiv =document.createElement('div');
     episodeDiv.className = "episodeDiv";
-    document.body.appendChild(episodeDiv)
+    document.body.appendChild(episodeDiv);
 
-  // The title and Episode comedy
+  // The title and Episode 
     let episodeTitle = document.createElement('h2');
     episodeTitle.className = "episodeTitle";
     episodeTitle.innerHTML = `${allEpisodes.name} ${episodeCode}`;
@@ -60,10 +74,13 @@ function display(allEpisodes) {
   
   }
 
-  let searchResults = document.getElementById('searchResults');
+ 
 
-  searchBar.addEventListener('keyup', function(e){
-    let searchTerm = e.target.value.toLowerCase();
+  searchBar.addEventListener('keyup', function(el){
+    let searchTerm = el.target.value.toLowerCase();
+    
+    
+   
   
     let filteredEpisodes = allEpisodes.filter((episode) => {
   
@@ -80,7 +97,7 @@ function display(allEpisodes) {
 
     const displayCharacters = (episodes) => {
 
-    const htmlString = episodes.map((episodes) => {
+    const htmlIndex = episodes.map((episodes) => {
   
         if (episodes.number <= 9 && episodes.season <= 9) {
           episodeCode = `S0${episodes.season}E0${episodes.number}`;
@@ -102,8 +119,11 @@ function display(allEpisodes) {
       });
      
   
-    searchResults.innerHTML = `Displaying ${htmlString.length} of 73`;
-    episodeFrames.innerHTML = htmlString;
+    searchResults.innerHTML = `Displaying ${htmlIndex.length} of 73`;
+    episodeFrames.innerHTML = htmlIndex;
     
 
+    
     }
+
+ 
